7eb0005de48098e3e58addde3fd0e3798470fc38
Rocco Gioia <109048248+lolxd122323@users.noreply.github.com>
2022-08-19 11:27:04 -0300

    Add files via upload

---
+++ lol/ejercicio7/ejercicio9.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888'+pedido.url)
+  console.log(url)
+  respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+  respuesta.write('<!doctype html><html><head></head><body>Hola mundo</body></html>')
+  respuesta.end()
+});
+
+servidor.listen(8088)
+
+console.log('Servidor web iniciado')
